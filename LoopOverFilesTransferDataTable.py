135891882c0626c0fb6cdb97bec4d4cf7ee597a2
Kushal Jhunjhunwalla <kushal.jhunjhunwalla@gmail.com>
2017-11-01 11:18:02 -0700

    Update script for better error handling

--- LoopOverFilesTransferDataTable.py
+++ LoopOverFilesTransferDataTable.py
@@ -3,7 +3,7 @@
 """
 Created on Mon Oct 16 12:27:34 2017
 
-@author: Anthony Yorita
+@authors: Anthony Yorita, Kushal Jhunjhunwalla
 """
 
 #LoopOverFilesTransferDataTable
@@ -12,48 +12,47 @@ import docx
 import re
 import openpyxl
 import transferDataTableMultipleFiles
-NumberOfRows = 2
+InitialRow = 2
 # For windows the format will be in - > C:\\Users\\asweigart
-# Need to locate the correct directory/file
-# os.chdir format for windows is C:\\Users\\... it needs to two backslashes
+# Need to locate the correct directory/file 
+# os.chdir format for windows is C:\\U1sers\\... it needs to two backslashes
 # wheras mac needs 1 forward slash (/)
-#
+# 
 #os.chdir('/Users/Gesetze/documents/Data_Science_Internship/Files_To_Transfer')
-os.chdir('F:\\NIBN_Leads_oRIGINAL') # for windows
+
+# **** NEED TO ALWAYS CHANGE DIRECTORY
+os.chdir('/Volumes/Elements/Lexi_Organized_NIBIN_Leads/Lexi_Organized_NIBIN') # Mac format
 #print(os.getcwd())
 
 # for wbLoad, same as the top, make sure the format excel file is already created
 # all the variables need to be on row 1
 
-# wbLoad needs a template excel file
+# wbLoad needs a template excel file 
 # Below for test files
-wbLoad = 'C:\\Script\\Ice_Cream\\NLN.xlsx'
+#wbLoad = '/Users/Gesetze/documents/Data_Science_Internship/NLN.xlsx' # Mac format
+wbLoad = 'F:\\NIBN_Leads\\Lead_TEST_DocumentVersion3.xlsx' # Windows format
 
-#wbLoad = 'F:\\NIBN_Leads\\Lead_TEST_DocumentVersion3.xlsx'
+
 total_files = 0
 total_files_errors = 0.0
 file_errors_list = []
-# change os.listdir depending on where the file is
-#for filename in os.listdir('/Users/Gesetze/documents/Data_Science_Internship/Files_To_Transfer'):
-for filename in os.listdir('F:\\NIBN_Leads_oRIGINAL'):
-#for filename in os.listdir('F:\\NIBN_Leads'):
+# change os.listdir depending on where the file is 
+for filename in os.listdir('/Volumes/Elements/Lexi_Organized_NIBIN_Leads/Lexi_Organized_NIBIN'): # Mac format
+#for filename in os.listdir('F:\\NIBN_Leads'): # Windows format
     if filename.endswith('.docx') and '~' not in filename:
-
-        try: # print(os.path.join(directory, filename))
-            # print(filename)
-
-            #workbook to load
-            executeFile = transferDataTableMultipleFiles.transferData(filename, NumberOfRows, wbLoad)
-            #print(executeFile)
+        
+        try:             
+        # workbook to load
+            executeFile = transferDataTableMultipleFilesFixTables.transferData(filename, InitialRow, wbLoad)
+            # print(executeFile)
             wbLoad = executeFile[1]
-            NumberOfRows = NumberOfRows + int(executeFile[0] - 1)
+            InitialRow = InitialRow + int(executeFile[0])
             print(filename)
-        except Exception as e:
+        except:
             print('There is an error at file :' + filename)
-            print(e)
             total_files_errors += 1
-            #file_errors_list.append(filename)
-            #break
+            file_errors_list.append(filename)
+            # break """
     total_files += 1
 print('There were ' + str(total_files_errors) + ' files that had errors')
 print('Total completed rate: ' + str((total_files - total_files_errors)/ total_files))
